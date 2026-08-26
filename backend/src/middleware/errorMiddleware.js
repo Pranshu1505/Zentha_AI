88e276b59a54a708cd7adf0b512ff9477ddba49e
Pranshu1505 <pranshubarhat@email.com>
2026-08-26 11:30:52 +0530

    Add error logging

--- backend/src/middleware/errorMiddleware.js
+++ backend/src/middleware/errorMiddleware.js
@@ -4,6 +4,7 @@ export const notFound = (req, res, next) => {
 };
 
 export const errorHandler = (err, req, res, next) => {
+  console.error(err);
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
     message: err.message,
